ae8ea26959a5601ef85e26f5b9915531cd4f13a0
Mohamed Shaman <it19029900@my.sliit.lk>
2022-12-25 23:02:52 +0530

    02 - 3. Creating Routes  Responses In Express

--- Api/server.js
+++ Api/server.js
@@ -6,6 +6,26 @@ dotenv.config({ path: "./config/config.env" });
 
 const app = express();
 
+app.get("/api/v1/bootcamps", (req, res) => {
+  res.status(200).json({ success: true, msg: "show all bootcamps" });
+});
+
+app.post("/api/v1/bootcamps", (req, res) => {
+  res.status(200).json({ success: true, msg: "Create new bootcamps" });
+});
+
+app.put("/api/v1/bootcamps/:id", (req, res) => {
+  res
+    .status(200)
+    .json({ success: true, msg: `Update bootcamp ${req.params.id}` });
+});
+
+app.delete("/api/v1/bootcamps/:id", (req, res) => {
+  res
+    .status(200)
+    .json({ success: true, msg: `Delete bootcamp ${req.params.id}` });
+});
+
 const PORT = process.env.PORT || 5000;
 
 app.listen(
