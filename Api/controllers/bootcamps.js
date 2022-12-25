a4d21f53307e12482496dfc69d818e1fbec202dd
zamanmohamed <zamanmohamed97@gmail.com>
2022-12-26 02:54:19 +0530

    04 - 5. Create Bootcamp - POST

--- Api/controllers/bootcamps.js
+++ Api/controllers/bootcamps.js
@@ -1,3 +1,5 @@
+const Bootcamp = require("../models/Bootcamp");
+
 // @desc      Get all bootcamps
 // @route     GET /api/v1/bootcamps
 // @access    Public
@@ -17,7 +19,18 @@ exports.getBootcamp = (req, res, next) => {
 // @desc      Create new bootcamp
 // @route     POST /api/v1/bootcamps
 // @access    Private
-exports.createBootcamp = (req, res, next) => {
+exports.createBootcamp = async (req, res, next) => {
+  try {
+    const bootcamp = await Bootcamp.create(req.body);
+
+    res.status(201).json({
+      success: true,
+      data: bootcamp,
+    });
+  } catch (err) {
+    res.status(401).json({ success: false });
+  }
+
   res.status(200).json({ success: true, msg: "Create new bootcamps" });
 };
 
